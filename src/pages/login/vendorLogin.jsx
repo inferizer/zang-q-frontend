154dd416c4b0abbbf73a334f22d115254a3af61e
PharadonSPK <icecream_2012@hotmail.com>
2023-11-01 16:35:31 +0700

    DoneregisterloginVender

--- src/pages/login/vendorLogin.jsx
+++ src/pages/login/vendorLogin.jsx
@@ -20,19 +20,63 @@ export default function VendorLogin() {
         {inputTag.map((el) => {
           return (
             <>
-              <label>{el.name}</label>
-              <Input
-                key={el.id}
-                name={el.name}
-                type="text"
-                placeholder="enter here"
-                onChange={hdl_input}
-              />
+              <div className="mobile">
+                <header className="mobile">
+                  <div className="mobile:  justify-center items-center h-screen flex">
+                    <form className="mobile">
+                      <h1 className="mobile: mb-[25px]">
+                        <b className="mobile">Saved</b>
+                      </h1>
+                      <div className="mobile: border-solid border-2 mb-4 border-[#BDBDBD]">
+                        <label className="mobile" for="email"></label>
+                        <input
+                          onChange={hdl_input}
+                          className="mobile: w-[367px]"
+                          placeholder="teenoi_chulapark@gmail.com"
+                          type="text"
+                          id="email"
+                          name="email"
+                        />
+                      </div>
+                      <div className="mobile: border-solid border-2 border-[#BDBDBD] ">
+                        <label className="mobile" for="password"></label>
+                        <input
+                          onChange={hdl_input}
+                          className="mobile: w-[367px]"
+                          type="password"
+                          placeholder="Enter Password"
+                          name="password"
+                          required
+                        />
+                        {/* mobile:mt-[25px] w-[350px] flex justify-center items-center */}
+                      </div>
+                      <button
+                        onClick={hdl_vendor_login_submit}
+                        type="button"
+                        className="mobile: mt-[25px] ml-[9px] w-[350px] flex justify-center items-center focus:outline-none text-white bg-[#56CAAE] hover:bg-teal-400 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 "
+                      >
+                        Continue
+                      </button>
+                      <div className="mobile: inline-flex items-center justify-center w-full">
+                        <hr className="mobile: w-64 h-px my-8 bg-gray-300 border-0 dark:bg-gray-700" />
+                        <span className="mobile: absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
+                          or
+                        </span>
+                      </div>
+
+                      <div className="mobile: text-center">
+                        <span className="mobile">Don't have an account?</span>
+                        <span className="mobile: ml-4">
+                          <u className="mobile">Sign up</u>
+                        </span>
+                      </div>
+                    </form>
+                  </div>
+                </header>
+              </div>
             </>
           );
         })}
-
-        <button>login</button>
       </form>
     </>
   );
--- src/pages/register/vendorRegister.jsx
+++ src/pages/register/vendorRegister.jsx
@@ -1,47 +1,82 @@
+import { useAuth } from "../../hook/useAuthContext";
+import { useNavigate } from "react-router-dom";
+import Input from "../../component/input";
+export default function VendorRegistor() {
+  const { hdl_vendor_register_submit, hdl_input } = useAuth();
+  const navigate = useNavigate();
 
-import {useAuth} from "../../hook/useAuthContext"
-import {useNavigate} from "react-router-dom"
-import Input from "../../component/input"
-export default function VendorRegistor(){
-    
-    const {hdl_vendor_register_submit,hdl_input,} = useAuth()
-    const navigate = useNavigate()
+  const inputTag = [
+    { name: "email" },
+    { name: "password" },
+    { name: "confirmPassword" },
+  ];
 
+  const hdl_submit = async (e) => {
+    e.preventDefault();
+    await hdl_vendor_register_submit()
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
 
-    const inputTag = [
-        { name: "email" },
-        { name: "password" },
-        { name: "confirmPassword" }
-    ]
-
-    const hdl_submit =  async (e) =>{
-        e.preventDefault()
-        await hdl_vendor_register_submit().then( ()=>{
-            navigate('/')
-        }).catch(err=>{
-            alert(err.response.data.message)
-        })
-    
-
-    }
-
-    return (
-        <>
-            <form className=' flex flex-col' onSubmit={hdl_submit}>
-
-                {inputTag.map((el, id) => {
-                    return (
-                        <>
-                            <label>{el.name}</label>
-                            <Input id={id} name={el.name} type='text' placeholder="enter here" onChange={hdl_input} />
-                        </>
-                    )
-                }
-                )
-
-                }
-                <button>submit</button>
+  return (
+    <>
+      <div className="mobile">
+        <header className="mobile">
+          <div className="mobile:flex justify-center items-center h-screen flex">
+            <form className="mobile">
+              <h1 className="mobile:mb-[25px]">
+                <b className="mobile">register form</b>
+              </h1>
+              <div className="mobile:border-solid border-2 mb-4 border-[#BDBDBD]">
+                <label className="mobile" for="email"></label>
+                <input
+                  onChange={hdl_input}
+                  className="mobile:w-[367px]"
+                  placeholder="email@gmail.com"
+                  type="text"
+                  id="email"
+                  name="email"
+                />
+              </div>
+              <div className="mobile: border-solid border-2 border-[#BDBDBD] ">
+                <label className="mobile" for="password"></label>
+                <input
+                  onChange={hdl_input}
+                  className="mobile: w-[367px]"
+                  type="password"
+                  placeholder="EnterPassword"
+                  name="password"
+                  required
+                />
+              </div>
+              <div className="mobile: mt-[15px]">
+                <div className="mobile: border-solid border-2 border-[#BDBDBD] ">
+                  <label className="mobile" for="confirmPassword"></label>
+                  <input
+                    onChange={hdl_input}
+                    className="mobile: w-[367px]"
+                    type="password"
+                    placeholder="ConfirmPassword"
+                    name="confirmPassword"
+                    required
+                  />
+                </div>
+              </div>
+              <button
+                onClick={hdl_vendor_register_submit}
+                type="button"
+                className="mobile: mt-[300px] ml-[9px] w-[350px] flex justify-center items-center focus:outline-none text-white bg-[#56CAAE] hover:bg-teal-400 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 "
+              >
+                Continue
+              </button>
             </form>
-        </>
-    )
+          </div>
+        </header>
+      </div>
+    </>
+  );
 }
