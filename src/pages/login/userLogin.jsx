acac032b8c785eb3f193e20a73029f5a9e2d4d19
Kullathat <jamesalike@hotmail.com>
2023-11-06 14:41:52 +0700

    frontendbackup

--- src/pages/login/userLogin.jsx
+++ src/pages/login/userLogin.jsx
@@ -5,8 +5,6 @@ import Input from "../../component/input";
 
 import LineLoginButton from "../../component/LineLoginButton";
 import liff from '@line/liff'
-<<<<<<< HEAD
-import { useEffect } from "react";
 export default function UserLogin(){
     
   const {hdl_input,input,hdl_user_login_submit,SuccessGoogle,failGoogle} = useAuth()
@@ -14,25 +12,6 @@ export default function UserLogin(){
     liff.init({ liffId:'2001390459-y9GRwgp7'})
      const clientId = "25769644481-it9q9s3alpf30c274qlqgepm7tppqr07.apps.googleusercontent.com"
  
-=======
-<<<<<<< HEAD
-
-export default function UserLogin() {
-    const { hdl_input, input, hdl_user_login_submit } = useAuth()
-
-    liff.init({ liffId: '2001390459-y9GRwgp7' })
-
-=======
-import { useEffect } from "react";
-export default function UserLogin(){
-    
-  const {hdl_input,input,hdl_user_login_submit,SuccessGoogle,failGoogle} = useAuth()
-
-    liff.init({ liffId:'2001390459-y9GRwgp7'})
-     const clientId = "25769644481-it9q9s3alpf30c274qlqgepm7tppqr07.apps.googleusercontent.com"
- 
->>>>>>> 1dffeed4e58c54950ca8b38841ad8fa56cf0d57d
->>>>>>> develop
     const inputTag = [
         { id: 4, name: "emailOrMobile" },
         { id: 2, name: "password" },
@@ -57,29 +36,6 @@ export default function UserLogin(){
 
                 }
 
-<<<<<<< HEAD
-      
-<GoogleLogin 
-clientId={clientId}
-buttonText="sign in with google"
-onSuccess={SuccessGoogle}
-onFailure={failGoogle}
-cookiePolicy="single_host_origin"
-isSignedIn={false}
-=======
-<<<<<<< HEAD
-                <button>login</button>
->>>>>>> develop
-
-/>
-
-<<<<<<< HEAD
-      </form>  
-                <LineLoginButton/>
-=======
-            </form>
-            <LineLoginButton />
-=======
       
 <GoogleLogin 
 clientId={clientId}
@@ -93,15 +49,9 @@ isSignedIn={false}
 
       </form>  
                 <LineLoginButton/>
->>>>>>> 1dffeed4e58c54950ca8b38841ad8fa56cf0d57d
->>>>>>> develop
         </>
 
 
 
     )
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 1dffeed4e58c54950ca8b38841ad8fa56cf0d57d
