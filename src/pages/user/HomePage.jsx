69b8418fa30fd3519295d90fc0ae9b094dfabb4b
Kullathat <jamesalike@hotmail.com>
2023-11-07 16:32:15 +0700

    backupBeforePushLogin

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -1,11 +1,11 @@
-// //Homepage
-// import React from "react";
+//Homepage
+import React from "react";
 // import { myImage } from "../../assets/images/maps1.png";
-// export default function HomePage() {
-//   return;
-//   <div className="responsive">
-//     <div className="container max-width: 640px">
-//       <img src={myImage} alt="map" />
-//     </div>
-//   </div>;
-// }
+export default function HomePage() {
+  return;
+  <div className="responsive">
+    <div className="container max-width: 640px">
+      <img src={myImage} alt="map" />
+    </div>
+  </div>;
+}
