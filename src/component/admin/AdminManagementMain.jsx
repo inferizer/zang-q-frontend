5dd95ef0bb9dc300589ef72c00e72d5dac4d6b15
ParitaSriwasut <parita.sri0327@gmail.com>
2023-11-16 00:35:24 +0700

    saving

--- src/component/admin/AdminManagementMain.jsx
+++ src/component/admin/AdminManagementMain.jsx
@@ -7,8 +7,9 @@ import {
 } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { useAdmin } from "../../hook/useAdmin";
-import RejectModal from "../modals/RejectModal";
 import ApprovedModal from "../modals/ApprovedModal";
+import RejectModal from "../modals/RejectModal";
+import Modal from "../../component/modal";
 
 export default function AdminManagementMain() {
   const {
@@ -39,35 +40,37 @@ export default function AdminManagementMain() {
 
   return (
     <>
-      <section className="w-screen bg-gray-50 px-4">
-        <div className="max-w-[800px] m-auto desktop:max-w-[1024px]">
-          <div className="mobile:justify-center items-center h-screen flex">
-            <div className="mobile:px-7 py-2 mx-auto top-20 float-none text-white flex flex-col fixed bg-primary-300 text-center rounded-2xl">
-              <p className="text-lg">ร้านรอการตรวจสอบ</p>
-              {pendingVendor.map((el) => (
-                <div
-                  key={el.id}
-                  className="mobile:col-start-2 col-span-4 bg-primaryWhite shadow-lg shadow-gray20 rounded-lg py-2 px-6 mb-4"
-                >
-                  <div className="mobile:text-gray-700 font-semibold py-2">
-                    {el.shopName}
-                  </div>
-                  <div className="mobile:gap-2 flex justify-start">
-                    <RiMapPinFill className="mobile:text-primary-600 w-4 h-4 cursor-pointer" />
-                    <div className="mobile:font-bold text-xs text-gray-700">
-                      {el.address}
+      <div className="bg-white flex justify-center items-start h-screen">
+        <div className=" mobile:max-w-[800px] desktop:max-w-[1024px] flex flex-col  items-center p-4 rounded-lg gap-4 mt-20 bg-white w-[500px] border shadow-xl">
+          <h1>Pendding</h1>
+          <div>
+            {pendingVendor.map((el) => (
+              <div
+                key={el.id}
+                className=" flex items-center justify-between w-[400px] gap-12 py-4"
+              >
+                <div className="flex flex-col gap-2 ">
+                  <div className="text-gray-900">{el.shopName}</div>
+                  <div className="gap-2 flex justify-start">
+                    <div className="flex gap-2">
+                      <RiMapPinFill className="mobile:text-primary-600 text-xl cursor-pointer" />
+                      <div className="font-bold text-xs text-gray-700">
+                        {el.address}
+                      </div>
                     </div>
                   </div>
-                  <div className="mobile:flex justify-end gap-6 py-2 px-6">
+                </div>
+                <div>
+                  <div className="flex justify-end gap-6 py-2 px-6">
                     <Link
                       to={`admin/restaurant/details/${el.id}`}
                       onClick={() => hdl_view_pending_detail()}
                     >
-                      <RiEyeFill className="mobile:text-primary-600 w-4 h-4 cursor-pointer" />
+                      <RiEyeFill className="text-primary-600 text-xl cursor-pointer" />
                     </Link>
 
                     <RiCloseCircleFill
-                      className="mobile:text-primary-600 w-4 h-4 cursor-pointer"
+                      className="text-primary-600 text-xl cursor-pointer"
                       onClick={() => {
                         hdl_reject_application();
                         handleRejectModalOpen();
@@ -75,7 +78,7 @@ export default function AdminManagementMain() {
                     />
 
                     <RiCheckboxCircleFill
-                      className="mobile:text-primary-600 w-4 h-4 cursor-pointer"
+                      className="text-primary-600 text-xl cursor-pointer"
                       onClick={() => {
                         hdl_approved_submit();
                         handleApproveModalOpen();
@@ -83,15 +86,79 @@ export default function AdminManagementMain() {
                     />
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
-      </section>
-      {isRejectModalOpen && <RejectModal onClose={handleRejectModalClose} />}
-      {isApproveModalOpen && (
-        <ApprovedModal onClose={handleApproveModalClose} />
-      )}
+      </div>
     </>
   );
 }
+
+// import React, { useState } from "react";
+// import {
+//   RiCheckboxCircleFill,
+//   RiCloseCircleFill,
+//   RiEyeFill,
+//   RiMapPinFill,
+// } from "react-icons/ri";
+// import { Link } from "react-router-dom";
+// import { useAdmin } from "../../hook/useAdmin";
+// import Modal from "../../component/modal";
+
+// export default function AdminManagementMain() {
+//   const {
+//     hdl_approved_submit,
+//     hdl_view_pending_detail,
+//     hdl_reject_application,
+//     pendingVendor,
+//     singlePendingVendor
+//   } = useAdmin();
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   if(isOpen) return < Modal onClose={()=>{
+//     setIsOpen(false)
+// }}>
+//         <div>
+
+//         </div>
+
+// </Modal>
+
+// {pendingVendor.map((el) => (
+//   <div key={el.id} className="py-4 px-6">
+//     <div className="text-gray-900 items-start justify-start flex">
+//       {el.shopName}
+//     </div>
+//     <div className="gap-2 flex justify-start">
+//       <RiMapPinFill className="mobile:text-primary-600 w-4 h-4 cursor-pointer" />
+//       <div className="font-bold text-xs text-gray-700">
+//         {el.address}
+//       </div>
+//     </div>
+//     <div className="flex justify-end gap-6 py-2 px-6">
+//       <Link
+//         to={`admin/restaurant/details/${el.id}`}
+//         onClick={() => hdl_view_pending_detail()}
+//       >
+//         <RiEyeFill className="text-primary-600 w-4 h-4 cursor-pointer" />
+//       </Link>
+
+//       <RiCloseCircleFill
+//         className="text-primary-600 w-4 h-4 cursor-pointer"
+//         onClick={() => {
+//           hdl_reject_application();
+//           handleRejectModalOpen();
+//         }}
+//       />
+
+//       <RiCheckboxCircleFill
+//         className="text-primary-600 w-4 h-4 cursor-pointer"
+//         onClick={() => {
+//           hdl_approved_submit();
+//           handleApproveModalOpen();
+//         }}
+//       />
+//     </div>
+//   </div>
+// ))}
