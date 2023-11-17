621919898d085715390baac6de0b07107e6f1a6a
inferizer <codecamp.1412@gmail.com>
2023-11-17 16:48:21 +0700

    prepare merge develop

--- src/pages/vendor/VendorHome.jsx
+++ src/pages/vendor/VendorHome.jsx
@@ -3,49 +3,52 @@ import axios from "../../config/axios";
 import { useEffect } from "react";
 import ShopBanner from "../../component/user-vendor_booking/ShopBanner";
 import VendorCard from "../../component/vendor/vendorCard";
+import { useQueue } from "../../hook/useQueue";
 export default function VendorHome() {
-  const [data, setData] = useState([])
-  const [shop, setShop] = useState()
-  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([]);
+  const [shop, setShop] = useState();
+  const [loading, setLoading] = useState(true);
+  const { shopInfo, setShopInfo } = useQueue();
 
   useEffect(() => {
+    console.log("userEffect");
     if (loading)
-      axios.get('/vendor/getMyShop')
-        .then(res => {
-          setShop(res.data.result)
-          console.log(res.data.result)
+      axios
+        .get("/vendor/getMyShop")
+        .then((res) => {
+          setShop(res.data.result);
         })
-        .catch(err => {
-          console.log(err)
+        .catch((err) => {
+          console.log(err);
         })
-        .finally(setLoading(false))
-
-  }, [])
+        .finally(setLoading(false));
+  }, []);
   useEffect(() => {
-    axios.get('/vendor/findallshop')
-      .then(res => {
-        setData(res.data.result)
-        console.log(res.data.result)
+    axios
+      .get("/vendor/findallshop")
+      .then((res) => {
+        setData(res.data.result);
+        console.log(res.data.result);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  console.log(shop)
+  // console.log(shopInfo);
 
   return (
     <section className='w-screen bg-gray-50 px-4 desktop:mt-[2rem]'>
       <div className='max-w-[800px] m-auto desktop:max-w-[1024px]'>
         <div className='mobile:justify-center items-center h-screen flex'>
-          <div >
+          <div>
             <ShopBanner
               name={shop && shop[0]?.shopName}
               src={shop && shop[0]?.shopPicture}
             />
-            <div >
+            <div>
               {data?.map((product, index) => (
-                <div key={index} >
+                <div key={index}>
                   <VendorCard
                     username={product.user?.username}
                     src={product.profileImage}
@@ -53,12 +56,11 @@ export default function VendorHome() {
                     type={product.type}
                   />
                 </div>
-              ))
-              }
+              ))}
             </div>
           </div>
-        </div> 
+        </div>
       </div>
     </section>
-  )
+  );
 }
